68c99391a550ebffa5c10b90c614476af22d94ee
ethanlagrone <ethan_lagrone@mines.edu>
2025-09-27 13:57:51 -0600

    Goblin test, very similar to bumblebee on example, seems to work

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -1,5 +1,61 @@
 package tests;
 
-public class TestInteractions {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.Goblin;
+
+public class TestInteractions {
+	
+	//test archer interactions
+	@Test
+	public void archerInteraction() {
+		//stub function
+	}
+	
+	//test canon interactions
+	@Test
+	public void canonInteraction() {
+		//stub function
+	}
+	
+	//test goblin interactions
+	@Test
+	public void goblinInteraction() {
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Goblin goblin = new Goblin(5);
+		gameBoard[5] = goblin;
+		
+		// Hit player if on same space
+		assertEquals(InteractionResult.HIT, goblin.interact(gameBoard, 5));
+		
+		// These loops ensure no interaction if not on same space
+		for (int i=0; i<5; i++)
+			assertEquals(InteractionResult.NONE, goblin.interact(gameBoard, i));
+		for (int i=6; i<GameEngine.BOARD_SIZE; i++)	
+			assertEquals(InteractionResult.NONE, goblin.interact(gameBoard, i));
+	}
+	
+	//test ninja interaction
+	@Test
+	public void ninjaInteraction() {
+		//stub function
+	}
+	
+	//test queen interaction 
+	@Test
+	public void queenInteraction() {
+		//stub function
+	}
+	
+	//test treasure interaction
+	@Test
+	public void treasureInteraction() {
+		//stub function
+	}
+	
 }
--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -1,5 +1,19 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
 public class TestMovingPieces {
+	
+	//Testing ninja movement
+	@Test
+	public void testMovingNinja() {
+		//stub
+	}
+	
+	//Testing Goblin movement
+	@Test
+	public void testMovingGoblin() {
+		//stub
+	}
 
 }
