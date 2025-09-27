046364f074ad5442367b5219105ca56a5769a3a6
ethanlagrone <ethan_lagrone@mines.edu>
2025-09-27 14:11:25 -0600

    Ninja, queen and treasure interaction test

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.Test;
 import gameEngine.Drawable;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
+import levelPieces.Canon;
 import levelPieces.Goblin;
+import levelPieces.Ninja;
+import levelPieces.Queen;
+import levelPieces.Treasure;
 
 public class TestInteractions {
 	
@@ -20,7 +24,7 @@ public class TestInteractions {
 	//test canon interactions
 	@Test
 	public void canonInteraction() {
-		//stub function
+		//Not sure how to test random hits
 	}
 	
 	//test goblin interactions
@@ -43,19 +47,49 @@ public class TestInteractions {
 	//test ninja interaction
 	@Test
 	public void ninjaInteraction() {
-		//stub function
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Ninja ninja = new Ninja(5);
+		gameBoard[5] = ninja;
+		
+		// Checks for interactions which should be none except for the three spaces around the ninja
+		for (int i=0; i<4; i++)
+			assertEquals(InteractionResult.NONE, ninja.interact(gameBoard, i));
+		for (int i=4; i<=6; i++)	
+			assertEquals(InteractionResult.HIT, ninja.interact(gameBoard, i));
+		for (int i=7; i<GameEngine.BOARD_SIZE; i++)	
+			assertEquals(InteractionResult.NONE, ninja.interact(gameBoard, i));
 	}
 	
 	//test queen interaction 
 	@Test
 	public void queenInteraction() {
-		//stub function
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Queen queen = new Queen(5);
+		gameBoard[5] = queen;
+		
+		assertEquals(InteractionResult.ADVANCE, queen.interact(gameBoard, 5));
+
+		// Checks for interactions which should be none except for the space on the queen
+		for (int i=0; i<5; i++)
+			assertEquals(InteractionResult.NONE, queen.interact(gameBoard, i));
+		for (int i=6; i<GameEngine.BOARD_SIZE; i++)	
+			assertEquals(InteractionResult.NONE, queen.interact(gameBoard, i));
 	}
 	
 	//test treasure interaction
 	@Test
 	public void treasureInteraction() {
-		//stub function
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Treasure treasure = new Treasure(5);
+		gameBoard[5] = treasure;
+		
+		assertEquals(InteractionResult.GET_POINT, treasure.interact(gameBoard, 5));
+
+		// Checks for interactions which should be none except for the space on the treasure
+		for (int i=0; i<5; i++)
+			assertEquals(InteractionResult.NONE, treasure.interact(gameBoard, i));
+		for (int i=6; i<GameEngine.BOARD_SIZE; i++)	
+			assertEquals(InteractionResult.NONE, treasure.interact(gameBoard, i));
 	}
 	
 }
