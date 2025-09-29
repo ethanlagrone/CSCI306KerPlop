15d286bd5e4374bd6b7d8e96531225d1fe932819
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2025-09-28 19:59:07 -0600

    Update Archer.java
    fixed logic error to do with the number of spaces on the board

--- src/levelPieces/Archer.java
+++ src/levelPieces/Archer.java
@@ -20,8 +20,8 @@ public class Archer extends GamePiece {
 		if(playerLocation < this.getLocation()) {
 			//Archer shoots at player if player in front of them
 			Random random = new Random();
-			int success = (10-Math.abs(playerLocation-this.getLocation()));
-			int shot = random.nextInt(0, 20);
+			int success = (21-Math.abs(playerLocation-this.getLocation()));
+			int shot = random.nextInt(0, 42);
 			if(shot <= success) {
 				return InteractionResult.KILL;
 			} else {
